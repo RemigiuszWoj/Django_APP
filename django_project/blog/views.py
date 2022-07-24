48046c3026448886fac25e01e63ee74ec01b1bb7
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2022-07-24 12:57:02 +0200

    template

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Remigiusz",
+        "title": "Blog Post1",
+        "content": "First Post coctent",
+        "date_posted": "24.07.2022",
+    },
+    {
+        "author": "Wojewodzki",
+        "title": "Blog Post2",
+        "content": "Second Post coctent",
+        "date_posted": "24.07.2022",
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context ={
+        "post": posts
+    }
+    return render(request=request, template_name='blog/home.html', context=context)
 
 def abotu(request):
     return HttpResponse('<h1>About</h1>')
