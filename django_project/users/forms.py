db50879efb05c1be620a67bdc7cf269b9f089f8c
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2022-08-01 20:49:11 +0200

    profile modification

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
+from .models import Profile
 
 class UserRegisterForm(UserChangeForm):
     password = None
@@ -10,3 +11,17 @@ class UserRegisterForm(UserChangeForm):
         model = User
         # fields = ["username", "email", "password1", "passwors2"]
         fields = ["username", "email"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
