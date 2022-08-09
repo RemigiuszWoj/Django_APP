28bfec4caf0685d20cec5f8fc2430560187bd730
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2022-08-09 19:19:34 +0200

    reset password

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -136,3 +136,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOSTT = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 1234
+EMAILHOST_PASSWORD = 1234
